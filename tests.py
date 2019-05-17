6daf6ab02114fca65c83e58bc2fac1de49fef16e
vsilent <jabberroid@gmail.com>
2019-05-17 10:50:30 +0300

    tests for nginx, mysql, elasticsearch containers

--- tests.py
+++ tests.py
@@ -9,43 +9,46 @@ client = docker.from_env()
 
 time.sleep(10)
 for c in client.containers.list():
-    assert c.status == 'running'
     print(c.name)
     print(c.status)
 
-# NGINX
-nginx = client.containers.get('nginx')
-nginx_cfg = nginx.exec_run("/usr/sbin/nginx -T")
-assert nginx.status == 'running'
-# print(nginx_cfg.output.decode())
-assert 'server_name _;' in nginx_cfg.output.decode()
-assert "error_log /proc/self/fd/2" in nginx_cfg.output.decode()
-assert "location = /.well-known/acme-challenge/" in nginx_cfg.output.decode()
-assert 'HTTP/1.1" 500' not in nginx.logs()
+node = client.containers.get('node')
+print(node.logs())
 
-# test restart
-nginx.restart()
-time.sleep(3)
-assert nginx.status == 'running'
 
-# Symfony PHP
-php = client.containers.get('wordpress')
+# PHP-FPM
+php = client.containers.get('akeneo')
 assert php.status == 'running'
 php_conf = php.exec_run("php-fpm7.0 -t")
-# print(php_conf.output.decode())
+print(php_conf.output.decode())
 assert 'configuration file /etc/php/7.0/fpm/php-fpm.conf test is successful' in php_conf.output.decode()
 php_proc = php.exec_run("sh -c 'ps aux |grep php-fpm'")
 print(php_proc.output.decode())
 assert 'php-fpm: master process (/etc/php/7.0/fpm/php-fpm.conf)' in php_proc.output.decode()
-
 assert 'fpm is running, pid' in php.logs()
-# response = requests.get("http://localhost")
-# assert response.status_code == 200
 
-mysql = client.containers.get('wpdb')
-assert mysql.status == 'running'
-mycnf = mysql.exec_run("/usr/sbin/mysqld --verbose  --help")
-assert '/usr/sbin/mysqld  Ver 5.7.26' in mycnf.output.decode()
-mysql_log = mysql.logs()
+nginx_cfg = php.exec_run("/usr/sbin/nginx -T")
+print(nginx_cfg.output.decode())
+assert 'server_name _;' in nginx_cfg.output.decode()
+assert "error_log /proc/self/fd/2" in nginx_cfg.output.decode()
+assert "location = /.well-known/acme-challenge/" in nginx_cfg.output.decode()
+assert 'HTTP/1.1" 500' not in php.logs()
+
+db = client.containers.get('db')
+assert db.status == 'running'
+mycnf = db.exec_run("/usr/sbin/mysqld --verbose  --help")
+assert '/usr/sbin/mysqld  Ver 5.7' in mycnf.output.decode()
+mysql_log = db.logs()
 assert "mysqld: ready for connections" in mysql_log.decode()
 print(mysql_log.decode())
+
+# Elasticsearch
+elastic = client.containers.get('akeneo_search')
+assert elastic.status == 'running'
+port = client.api.inspect_container('elasticsearch')['NetworkSettings']['Ports']['9200/tcp'][0]['HostPort']
+response = requests.get("http://localhost:{}".format(port))
+assert '"name" : "elasticsearch"' in response.text
+assert '"number" : "5.4.3"' in response.text
+assert response.status_code == 200
+assert ' bound_addresses {0.0.0.0:9200}' in elastic.logs()
+
