18d37cc34b8f9c3b373d6a9f93d4bc7eb10b1c47
vsilent <jabberroid@gmail.com>
2019-05-24 09:33:50 +0300

    docker-compose run akeneo commands under akenep user

--- v3_0/scripts/init.sh
+++ v3_0/scripts/init.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
+
 # from pim project
 set -e
-cd "$(dirname "$0")"
 cd ../
 
 
-docker exec -it akeneo bash -c "php -d memory_limit=3G /home/akeneo/composer.phar update"
+docker-compose --user=akeneo exec akeneo bash -c "php -d memory_limit=3G composer.phar update"
 docker-compose -f docker-compose-build.yml run --rm node yarn install
 
 docker exec -it akeneo rm -rf var/cache/*
-docker exec -it akeneo bin/console --env=prod pim:installer:assets --symlink --clean
+docker-compose --user=akeneo exec akeneo bin/console --env=prod pim:installer:assets --symlink --clean
 docker-compose -f docker-compose-build.yml run --rm node yarn run webpack-dev
 
-docker exec -it akeneo rm -rf var/cache/*
-docker exec -it akeneo bin/console --env=prod pim:install --force --symlink --clean
+docker-compose --user akeneo exec akeneo rm -rf var/cache/*
+docker-compose --user akeneo exec akeneo bin/console --env=prod pim:install --force --symlink --clean
 docker-compose -f docker-compose-build.yml run --rm node yarn run webpack-dev
