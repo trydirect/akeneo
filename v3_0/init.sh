bb6ee1687f31395ff56378acdc7c613afb2a77a2
vsilent <jabberroid@gmail.com>
2019-05-24 13:04:19 +0300

    build backend first

--- v3_0/init.sh
+++ v3_0/init.sh
@@ -3,13 +3,12 @@
 # from pim project
 set -e
 
-docker-compose -f docker-compose-build.yml run --rm node yarn install
 docker-compose -f docker-compose-build.yml exec --user=travis akeneo php -d memory_limit=3G composer.phar update
-
 docker-compose -f docker-compose-build.yml exec --user=travis akeneo rm -rf var/cache/*
 docker-compose -f docker-compose-build.yml exec --user=travis akeneo bin/console --env=prod pim:installer:assets --symlink --clean
-docker-compose -f docker-compose-build.yml run --rm node yarn run webpack-dev
-
 docker-compose -f docker-compose-build.yml exec --user=travis akeneo rm -rf var/cache/*
 docker-compose -f docker-compose-build.yml exec --user=travis akeneo bin/console --env=prod pim:install --force --symlink --clean
+
+docker-compose -f docker-compose-build.yml run --rm node yarn install
+docker-compose -f docker-compose-build.yml run --rm node yarn run webpack-dev
 docker-compose -f docker-compose-build.yml run --rm node yarn run webpack-dev
