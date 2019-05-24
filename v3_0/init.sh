545e8a2347b26afccf5c023e6b8965c63967ebcd
vsilent <jabberroid@gmail.com>
2019-05-24 14:07:28 +0300

    build without travis user...

--- v3_0/init.sh
+++ v3_0/init.sh
@@ -3,11 +3,11 @@
 # from pim project
 set -e
 
-docker-compose -f docker-compose-build.yml exec --user=travis akeneo php -d memory_limit=3G composer.phar update
-docker-compose -f docker-compose-build.yml exec --user=travis akeneo rm -rf var/cache/*
-docker-compose -f docker-compose-build.yml exec --user=travis akeneo bin/console --env=prod pim:installer:assets --symlink --clean
-docker-compose -f docker-compose-build.yml exec --user=travis akeneo rm -rf var/cache/*
-docker-compose -f docker-compose-build.yml exec --user=travis akeneo bin/console --env=prod pim:install --force --symlink --clean
+docker-compose -f docker-compose-build.yml exec akeneo php -d memory_limit=3G composer.phar update
+docker-compose -f docker-compose-build.yml exec akeneo rm -rf var/cache/*
+docker-compose -f docker-compose-build.yml exec akeneo bin/console --env=prod pim:installer:assets --symlink --clean
+docker-compose -f docker-compose-build.yml exec akeneo rm -rf var/cache/*
+docker-compose -f docker-compose-build.yml exec akeneo bin/console --env=prod pim:install --force --symlink --clean
 
 docker-compose -f docker-compose-build.yml run --rm node yarn install
 docker-compose -f docker-compose-build.yml run --rm node yarn run webpack-dev
