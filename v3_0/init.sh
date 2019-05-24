ae99bf6ab9b97e2abba23b6901f31ba62dc961fa
vsilent <jabberroid@gmail.com>
2019-05-24 14:37:33 +0300

    create composer alias

--- v3_0/init.sh
+++ v3_0/init.sh
@@ -3,7 +3,7 @@
 # from pim project
 set -e
 
-docker-compose -f docker-compose-build.yml exec akeneo php -d memory_limit=3G composer.phar update
+docker-compose -f docker-compose-build.yml exec akeneo php -d memory_limit=3G composer update
 docker-compose -f docker-compose-build.yml exec akeneo rm -rf var/cache/*
 docker-compose -f docker-compose-build.yml exec akeneo bin/console --env=prod pim:installer:assets --symlink --clean
 docker-compose -f docker-compose-build.yml exec akeneo rm -rf var/cache/*
